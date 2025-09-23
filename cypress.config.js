d1052fb85526d3d3d10c4cbbe1c34ef7a42bcf18
NoToolsNoCraft <notoolsnocraft@gmail.com>
2025-09-23 09:40:03 +0200

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,16 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // This is the event listener for our custom 'log' task
+      on('task', {
+        log(message) {
+          console.log(message)
+          return null
+        },
+      })
     },
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
   },
-});
+})
